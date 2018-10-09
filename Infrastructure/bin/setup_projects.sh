055a83c563f879831fcb7018fdeabceb6f3e9100
P. Gomersbach <p.gomersbach@gmail.com>
2018-10-09 21:22:56 +0000

    remove clean

--- Infrastructure/bin/setup_projects.sh
+++ Infrastructure/bin/setup_projects.sh
@@ -9,9 +9,6 @@ fi
 GUID=$1
 USER=$2
 
-echo "clean for now"
-./Infrastructure/bin/cleanup.sh ${GUID}
-
 echo "Creating all Homework Projects for GUID=${GUID} and USER=${USER}"
 oc new-project ${GUID}-nexus        --display-name="${GUID} AdvDev Homework Nexus"
 oc new-project ${GUID}-sonarqube    --display-name="${GUID} AdvDev Homework Sonarqube"
