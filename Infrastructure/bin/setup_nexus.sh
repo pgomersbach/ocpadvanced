e6c1dc63be4d56291fcbe4f7eff992c5649f3041
P. Gomersbach <p.gomersbach@gmail.com>
2018-10-10 05:20:07 +0000

    add nexus-registry route from template

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -34,9 +34,9 @@ PROJ_NAME=${GUID}-${ITEM}
 oc project ${PROJ_NAME}
 echo "Create nexus from template"
 oc process -f Infrastructure/templates/nexus.yml -n ${PROJ_NAME} -p GUID=${GUID} | oc create -n ${PROJ_NAME} -f -
-echo "Expose nexus route via template"
+echo "Expose nexus routes via template"
 # oc expose svc nexus3 -n ${PROJ_NAME}
-oc expose svc ${ITEM}-registry -n ${PROJ_NAME}
+# oc expose svc ${ITEM}-registry -n ${PROJ_NAME}
 
 echo "Start waiting for ${ITEM} at";date
 
