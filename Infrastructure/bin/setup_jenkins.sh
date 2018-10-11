0cb4316ab8ab641e190b76bd60f3fe3f84077b1c
P. Gomersbach <p.gomersbach@gmail.com>
2018-10-11 10:37:03 +0000

    code cleanup jenkins

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -28,36 +28,14 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # * GUID: the GUID used in all the projects
 # * CLUSTER: the base url of the cluster used (e.g. na39.openshift.opentlc.com)
 
-
-##### Update nexus_settings file with right GUID and CLUSTER
-
-#sed -i "s/GUID/${GUID}/"     nexus_settings.xml
-#sed -i "s/CLUSTER{$CLUSTER}/"   nexus_settings.xml
-
-
-#####Create a Jenkins instance with persistent storage and sufficient resources
-
-
-#mkdir jenkins-slave-appdev
-
-#oc new-project $GUID-jenkins  --display-name "Shared Jenkins"
-
-#oc policy add-role-to-user admin ${USER} -n ${GUID}-jenkins
-
-#oc annotate namespace ${GUID}-jenkins openshift.io/requester=${USER} --overwrite
+echo "setup jenkins in ${GUID}-jenkins"
 oc project $GUID-jenkins
-#oc new-app -f ./Infrastructure/templates/jenkins.json --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param CPU_LIMIT=2
-#echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
-#oc -n $GUID-jenkins new-app -f Infrastructure/templates/jenkins.yml -p MEMORY_LIMIT=2Gi -p VOLUME_CAPACITY=4Gi
-#oc -n $GUID-jenkins rollout status dc/jenkins -w
-
-echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cluster ${CLUSTER}"
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=4Gi --param VOLUME_CAPACITY=4Gi -n ${GUID}-jenkins
 oc rollout pause dc jenkins -n ${GUID}-jenkins
 oc set resources dc jenkins --limits=memory=4Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
 oc rollout resume dc jenkins -n ${GUID}-jenkins
 
-
+echo "start check for jenkins"
 while : ; do
     oc get pod -n ${GUID}-jenkins | grep -v deploy | grep "1/1"
     echo "Checking if Jenkins is Ready..."
@@ -70,16 +48,12 @@ while : ; do
     fi
 done
 
-
-
 oc new-build --name=jenkins-slave-appdev --dockerfile=$'FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9\nUSER root\nRUN yum -y install skopeo apb && \yum clean all\nUSER 1001' -n ${GUID}-jenkins
 
+echo "start check for jenkins slave"
 while : ; do
-
-echo "Checking if Jenkins-app-slave is completed..."
-     
-oc get pod -n ${GUID}-jenkins | grep 'slave' | grep "Completed"
-     
+  oc get pod -n ${GUID}-jenkins | grep 'slave' | grep "Completed"
+  echo "Checking if Jenkins slave is Ready..."
     if [ $? == "0" ] 
       then 
         echo 'jenkins-slave-appdev build completed'
@@ -92,9 +66,6 @@ done
 
 
 oc create configmap basic-config --from-literal="GUID=${GUID}" --from-literal="REPO=${REPO}" --from-literal="CLUSTER=${CLUSTER}"
-
-#oc new-build --name=jenkins-slave-appdev   --dockerfile="$(< ./Infrastructure/templates/docker/skopeo/Dockerfile)"  -n $GUID-jenkins
-
 oc create -f Infrastructure/templates/bl-mlbparks.yml -n ${GUID}-jenkins
 oc create -f Infrastructure/templates/bl-nationalparks.yml -n ${GUID}-jenkins
 oc create -f Infrastructure/templates/bl-parksmap.yml -n ${GUID}-jenkins
@@ -102,23 +73,3 @@ oc create -f Infrastructure/templates/bl-parksmap.yml -n ${GUID}-jenkins
 oc set env bc/mlbparks-pipeline GUID=${GUID} REPO=${REPO} CLUSTER=${CLUSTER} -n ${GUID}-jenkins
 oc set env bc/nationalparks-pipeline GUID=${GUID} REPO=${REPO} CLUSTER=${CLUSTER} -n ${GUID}-jenkins
 oc set env bc/parksmap-pipeline GUID=${GUID} REPO=${REPO} CLUSTER=${CLUSTER} -n ${GUID}-jenkins
-
-
-# Deploy on DEV
-#cd  jenkins-slave-appdev
-#echo "FROM docker.io/openshift/jenkins-slave-maven-centos7:v3.9
-#USER root
-#RUN yum -y install skopeo apb && \
-#    yum clean all
-#USER 1001"  >  jenkins-slave-appdev/Dockerfile
-######  Set up three build configurations with pointers to the pipelines in the source code project. Each build configuration needs to point to the source code repository and the respective contextDir
-#cd jenkins-slave-appdev
-#docker build . -t docker-registry-default.apps.$CLUSTER/$GUID-jenkins/jenkins-slave-maven-appdev:v3.9
-#docker build . -t docker-registry-default.apps.$CLUSTER/$GUID-jenkins/jenkins-slave-maven-appuat:v3.9
-#docker build . -t docker-registry-default.apps.$CLUSTER/$GUID-jenkins/jenkins-slave-maven-appprod:v3.9
-#sudo docker login -u $GUID -p $(oc whoami -t) docker-registry-default.apps.$CLUSTER
-#sudo docker push docker-registry-default.apps.$CLUSTER/$GUID-jenkins/jenkins-slave-maven-appdev:v3.9
-#####     Create a build configuration to build the custom Maven slave pod to include Skopeo
-#skopeo copy --dest-tls-verify=false --dest-creds=$(oc whoami):$(oc whoami -t) docker-daemon:docker-registry-default.apps.$CLUSTER/$GUID-jenkins/jenkins-slave-maven-appdev:v3.9 docker://docker-registry-default.apps.$CLUSTER/$GUID-jenkins/jenkins-slave-maven-appdev:v3.9
-
-
